aa6748991d9ef64615ce4e073aecf69b93d7557c
Rohied Abdoella <74419576+RohiedAbdoella@users.noreply.github.com>
2021-01-26 22:28:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hallo Winc Academy studenten")
+
+/*
+Dit is een grote som. Deze comment zie je niet, omdat het wordt genegeerd door JS.
+*/
+let multiply = 1230941*1823794;
+console.log(multiply);
+
+let divide =  637263/54;
+console.log(divide);
+
+
+
